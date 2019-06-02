daf7cf34a30d3a5b9e63733e9c1721bdd99bdf05
theindianczar <theindianczar@gmail.com>
2019-06-02 18:44:54 +0530

    test commit

---
+++ converter.py
@@ -0,0 +1,15 @@
+num = 33
+print(bin(num))
+print(oct(num))
+print(hex(num))
+print(float(num))
+
+#converting binary to decimal
+i = int('100001',2)
+
+#example hex color DC 14 3C
+r = int('DC',16)
+g = int('14',16)
+b = int('3C',16)
+
+print(r,g,b)
--- daysConverter.py
+++ daysConverter.py
@@ -1,4 +1,4 @@
-days=int(input("enter the numberof days"))
+days=int(input("enter the number of days"))
 years = days//365
 weeks = (days%365)//7
 days_remaining = days-years*365-weeks*7
